6822b12fdb20667862abca4564df6103752265e6
Lucas Damas Corrêa <lucas.dcorrea1@gmail.com>
2021-08-17 00:35:30 -0300

    feat(DebitController): Init controller Debit

---
+++ SingleDebtControl/SingleDebtControl.Api/Controllers/DebitController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SingleDebtControl.Domain.Service.Debit;
+
+namespace SingleDebtControl.Api.Controllers
+{
+    [ApiController]
+    [Route("api/debit")]
+    public class DebitController : Controller
+    {
+        private readonly IDebitService _debitService;
+
+        public DebitController(IDebitService debitService)
+        {
+            _debitService = debitService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var debits = _debitService.Get();
+
+            return Ok(new { debits });
+        }
+
+    }
+}
