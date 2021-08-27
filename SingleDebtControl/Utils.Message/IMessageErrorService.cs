f6e02aed1ae8489bcc29861bfa4d0291be40c51b
Lucas Damas Corrêa <lucas.dcorrea1@gmail.com>
2021-08-26 23:42:18 -0300

    feat(MessageErrorService): Add domain error message

---
+++ SingleDebtControl/Utils.Message/IMessageErrorService.cs
@@ -0,0 +1,10 @@
+﻿namespace Utils.Message
+{
+    public interface IMessageErrorService
+    {
+        void Add(string message);
+        string GetMessageError();
+        T AddWithReturn<T>(string message);
+        bool Any();
+    }
+}
