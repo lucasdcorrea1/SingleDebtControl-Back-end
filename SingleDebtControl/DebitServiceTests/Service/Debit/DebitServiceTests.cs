c48b13333398e3825af519ae4b7752903f330c9f
Lucas Damas Corrêa <lucas.dcorrea1@gmail.com>
2021-08-30 23:18:07 -0300

    feat(Utils.Environments): Add environment variables

---
+++ SingleDebtControl/DebitServiceTests/Service/Debit/DebitServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SingleDebtControl.Domain.Service.Debit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DebitService.Tests
+{
+    [TestClass()]
+    public class DebitServiceTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ SingleDebtControl/Utils.Environments/Parameters.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Utils.Environments
+{
+    public class Parameters
+    {
+        public ParametersValues Data { get; set; }
+
+        public Parameters()
+        {
+            Data = GetData();
+        }
+
+        private ParametersValues GetData()
+        {
+            try
+            {
+                var parameters = new ParametersValues();
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+                var basePath = AppDomain.CurrentDomain.BaseDirectory;
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(basePath)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.{env}.json", optional: true)
+                    .AddEnvironmentVariables();
+
+                var configRoot = builder.Build();
+                configRoot.GetSection("Parameters").Bind(parameters);
+                parameters.Environment = env;
+
+                return parameters;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+
+    public class ParametersValues
+    {
+        public string ConnectionString { get; set; }
+        public string ConnectionStringHangfire { get; set; }
+        public string Environment { get; set; }
+    }
+
+}
